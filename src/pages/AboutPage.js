ffd08b5f5e5d7e402699b9da868ea626b0c9afa1
Adam <100063577+adam780930@users.noreply.github.com>
2022-03-05 21:14:54 -0800

    update

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,17 +1,22 @@
 import classes from './AboutPage.module.css';
-// import Portrait from '../components/Portrait';
 
 function AboutPage() {
     return (
         
-        <div className={classes.title}>
-            <img src='https://i.redd.it/jqt9q5252uj71.jpg' className={classes.portrait} alt=""></img>
-            <span className={classes.text}>
-                Hi, I am Adam
-            </span>
-            <p className={classes.text2}>A self-taught front-end-developer currently pursuing opportunities
-            to begin my career in the web developer industry.
-            </p>
+        <div className={classes.content}>
+            <img src='https://www.hlj.com/media/catalog/product/cache/467d58fefb7842fb1aed595e14a86f25/b/a/bans63358_0.jpg' 
+            className={classes.portrait} alt=""></img>
+            
+            <header className={classes.text}><span>Hi, I am Adam</span> <br></br>
+            A self-taught front-end-developer currently pursuing opportunities <br></br>
+            to begin my career in the web developer industry. <br></br>
+            I am currently located in Los Angeles, California. I can make websites
+            using HTML, CSS, JAVASCRIPT and REACT.JS <br></br>
+            I am always looking to learn more and open for all opportunities and projects.
+            
+            </header>
+
+
             
         </div>
     );
